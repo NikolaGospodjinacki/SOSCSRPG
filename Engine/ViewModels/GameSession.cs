1d23999a9f8e23a8471094ba0f77a9f07363afbc
Nikola Gospodjinacki <ngogank@gmail.com>
2022-02-14 14:25:11 +0100

    Added data binding for player stats. Added notification interface to player class.

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+            {
+                Name = "Nikola",
+                Class = "Mage",
+                HitPoints = 20,
+                ExperiencePoints = 0,
+                Level = 1,
+                Gold = 1000
+            };
+
+        }
+    }
+}
